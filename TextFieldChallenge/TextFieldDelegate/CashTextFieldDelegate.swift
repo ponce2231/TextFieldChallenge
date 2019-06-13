489e0ca21e3c11074a7fea3db713276af587ca8b
Christopher Ponce Mendez <christopherponcedev@gmail.com>
2019-06-13 12:57:18 -0400

    created the zipcode delegate and the cashtext field delegate

---
+++ TextFieldChallenge/TextFieldDelegate/CashTextFieldDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  CashTextFieldDelegate.swift
+//  TextFieldChallenge
+//
+//  Created by Christopher Ponce Mendez on 6/13/19.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+}
---
+++ TextFieldChallenge/TextFieldDelegate/ZipCodeTextFieldDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ZipCodeTextFieldDelegate.swift
+//  TextFieldChallenge
+//
+//  Created by Christopher Ponce Mendez on 6/13/19.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ZipCodeTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+}
